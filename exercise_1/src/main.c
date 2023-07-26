652faf2a230870258515f1a84df710b38166f19f
Capelli Ludovic <l.capelli@ed.ac.uk>
2023-07-26 20:38:27 +0100

    First two exercises added.

---
+++ exercise_1/src/main.c
@@ -0,0 +1,21 @@
+/**
+ * @file main.c
+ * @author Ludovic Capelli (l.capelli@epcc.ed.ac.uk)
+ * @brief This example is for you to practice a simple program compilation.
+ * @details Here are the compilation instructions, which you must execute from
+ * the exercise_1 folder:
+ * - Compile the program: gcc -o bin/main src/main.c
+ * - Execute the program: ./bin/main
+ **/
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * @brief Do not worry about understanding the function below. For now, the
+ * objective is simply to compile this source code.
+ */
+int main(int argc, char* argv[])
+{
+    printf("Compiling and executing a program: done.\n");
+    return EXIT_SUCCESS;
+}
---
+++ exercise_2/src/main.c
@@ -0,0 +1,31 @@
+/**
+ * @file main.c
+ * @author Ludovic Capelli (l.capelli@epcc.ed.ac.uk)
+ * @brief This example is for you to practice variable declarations.
+ * @details This program prints the value of two variables. However, these two
+ * variables are not declared. The objective is for you to declare the two
+ * variables and initialise them to the value indicated.
+ * To see the compilation and execution commands, refer to the first exercise.
+ * You are strongly encouraged to type them yourself, rather than copy/pasting,
+ * as it may help you memorise.
+ **/
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * @brief Do not worry about understanding the "int main(...)" line for now. You
+ * will learn it in due time, in a different exercise.
+ */
+int main(int argc, char* argv[])
+{
+    // Declare an integer called "year" and initialise it to 2023.
+
+    // Declare a double-precision variable called "pi" and initialise it to 3.14.
+
+    /* Do not modify the line below. In this exercise, you only declare the two
+     * variables needed, namely: "year" and "pi". You will learn how to print
+     * in an exercise later.
+     */
+    printf("The current year is %d. PI is approximately %f.\n", year, pi);
+    return EXIT_SUCCESS;
+}
