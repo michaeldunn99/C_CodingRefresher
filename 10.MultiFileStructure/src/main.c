aff7fc76d5bbd661b68eab3daeb7d4728e534d78
Capelli Ludovic <l.capelli@ed.ac.uk>
2023-08-01 16:44:20 +0100

    Part about multi-file moved to its own exercise.
    Unit 9 is only about function declarations, not multi-file structures. Therefore, the part about the multi-file structure has been moved to its own exercise as it is taught in the unit 10.

--- 10.MultiFileStructure/src/main.c
+++ 10.MultiFileStructure/src/main.c
@@ -1,8 +1,10 @@
 /**
  * @file main.c
  * @author Ludovic Capelli (l.capelli@epcc.ed.ac.uk)
- * @brief This example is for you to practice the decoupling of function
- * declaration and definition.
+ * @brief This example is for you to practice a multi-file structure.
+ * @details This exercise gets you to move a function outside of the file, 
+ * write the corresponding declaration in a header file, moving the definition
+ * to a source file and update the compilation command accordingly.
  * To see the compilation and execution commands, refer to the first exercise.
  * You are strongly encouraged to type them yourself, rather than copy/pasting,
  * as it may help you memorise.
@@ -20,9 +22,11 @@ int add_numbers(int a, int b)
  * will learn it in due time, in a different exercise.
  */
 int main(int argc, char* argv[])
-{
-    /* Provide the declaration of function add_numbers above the function main,
-     * and move the definition of function add_numbers below the function main.
+{    
+    /* Provide the declaration of function add_numbers in a header file, and
+     * move the definition of add_numbers outside of the file main.c, to the
+     * new source file you will create. Then, update the compilation command
+     * accordingly and execute the program.
      */
     int sum = add_numbers(123, 456);
 
