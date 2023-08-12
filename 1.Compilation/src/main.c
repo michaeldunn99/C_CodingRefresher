8023e2edd81f288bd2a96590dcce2461612d06f0
Capelli Ludovic <l.capelli@ed.ac.uk>
2023-08-12 15:46:56 +0200

    Documentation updated to reflect exercise objectives.

--- 1.Compilation/src/main.c
+++ 1.Compilation/src/main.c
@@ -2,18 +2,15 @@
  * @file main.c
  * @author Ludovic Capelli (l.capelli@epcc.ed.ac.uk)
  * @brief This example is for you to practice a simple program compilation.
- * @details Here are the compilation instructions, which you must execute from
- * the exercise_1 folder:
- * - Compile the program: gcc -o bin/main src/main.c
- * - Execute the program: ./bin/main
  **/
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
  * @brief Do not worry about understanding the function below. For now, the
- * objective is simply to compile this source code.
- */
+ * objective is simply to compile this source code and execute the binary
+ * produced.
+ **/
 int main(int argc, char* argv[])
 {
     printf("Compiling and executing a program: done.\n");
